d9f8add126020ab5cd5a4939ac46ef5db78198ab
J. Rook <jrook94@gmail.com>
2024-10-11 11:56:05 +0200

    Clean-up

--- src/robustranking/__init__.py
+++ src/robustranking/__init__.py
@@ -1,11 +1,2 @@
-# from .comparison.aggregated_comparison import AggregatedComparison
-# from .comparison.bootstrap_comparison import BootstrapComparison
-# from .comparison.mo_bootstrap_comparison import \
-#     MOBootstrapComparison
-# from .comparison.mo_domination_bootstrap_comparison import \
-#     MODominationBootstrapComparison
-# from .comparison.ranked_comparison import \
-#     RankedComparison
-# from .utils import metrics, multiobjective, plots
 from robustranking.benchmark import Benchmark
 from robustranking import comparison, utils
