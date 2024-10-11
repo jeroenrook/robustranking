978f49a1a81ccee8d8a6cac1920451baf789b458
J. Rook <jrook94@gmail.com>
2024-10-11 11:07:20 +0200

    Style

--- robustranking/__init__.py
+++ robustranking/__init__.py
@@ -1,8 +1,9 @@
-from .comparison.aggregated_comparison import AggregatedComparison
-from .comparison.bootstrap_comparison import BootstrapComparison
-from .comparison.mo_bootstrap_comparison import \
-    MOBootstrapComparison
-from .comparison.mo_domination_bootstrap_comparison import \
-    MODominationBootstrapComparison
-from .utils import metrics, multiobjective, plots
-from .benchmark import Benchmark
+# from .comparison.aggregated_comparison import AggregatedComparison
+# from .comparison.bootstrap_comparison import BootstrapComparison
+# from .comparison.mo_bootstrap_comparison import \
+#     MOBootstrapComparison
+# from .comparison.mo_domination_bootstrap_comparison import \
+#     MODominationBootstrapComparison
+# from .utils import metrics, multiobjective, plots
+from robustranking.benchmark import Benchmark
+from robustranking import comparison, utils
